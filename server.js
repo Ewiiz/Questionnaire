8cb264de40c489d0142692d06d923cdeedbaf16d
Bruno <bverpet46@gmail.com>
2023-05-28 10:58:54 +0400

    Ajouts des questions/réponses dans le fichier JSON

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.listen(PORT, () => {
     console.log(`Le serveur est démarrer sur le port: ${PORT}`)
 })
 
-app.get('/', (req, res) =>{
-    const questions = questionJS.question
-    const reponse = questionJS.reponse
-    res.send(`${questions}: ${reponse}`)
+app.get('/', (req, res) => {
+    const question1 = questionJS.question2
+    const reponse1 = questionJS.reponse2
+    res.send(`${question1}: ${reponse1}`)
 })
